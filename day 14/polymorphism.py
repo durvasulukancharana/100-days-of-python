61edc5b87aba63f75bb056c650c2191c2cee46b9
durvasulukancharana <durvasulukancharana25@gmail.com>
2025-08-05 19:36:48 +0530

    day 14 of ps

---
+++ day 14/polymorphism.py
@@ -0,0 +1,21 @@
+class sailu:
+    def mandal(self):
+        self.name="nandigam"
+        print(self.name)
+    def dist(self):
+        self.dist="srikakulam"
+        print(self.dist)
+class ganesh:
+    def mandal(self):
+        self.name="bobbili"
+        print(self.name)
+    def dist(self):
+        self.dist="vizaya nagaram"
+        print(self.dist)
+def x(y):
+    y.mandal()
+    y.dist()
+obj=sailu()
+x(obj)
+
+
