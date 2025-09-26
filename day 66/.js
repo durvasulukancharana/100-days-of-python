29f10fdb76ecb95f36edc2f3edbd40f28b74a618
durvasulukancharana <durvasulukancharana25@gmail.com>
2025-09-26 20:57:28 +0530

    day 66 of js

---
+++ day 66/.js
@@ -0,0 +1,142 @@
+// 1. What is the default value of a variable declared with var but not initialized?
+//  a) null
+//  b) 0
+//  c) undefined
+//  d) not defined
+
+// c. undefined
+
+
+// 2. Which error occurs if you try to use a variable that was never declared?
+//  a) ReferenceError: not defined
+//  b) SyntaxError
+//  c) TypeError
+//  d) undefined
+
+// a . not defind
+
+
+// 3. What will be the output?
+//  console.log(a);
+//  var a = 10;
+//  a) 10
+//  b) undefined
+//  c) not defined
+//  d) Error
+
+// b . undefined
+
+
+// 4. What is the scope of a variable declared with var inside a function?
+//  a) Block scope
+//  b) Function scope
+//  c) Global scope
+//  d) Module scope
+
+// b . function scope
+
+
+// 5. Which of the following statements is true about undefined?
+//  a) It means the variable was never declared
+//  b) It means the variable exists but no value is assigned
+//  c) It always means null
+//  d) It is the same as 'not defined'
+
+// b
+
+
+// 6. What will happen?
+//  console.log(b);
+//  (when b is never declared anywhere)
+//  a) undefined
+//  b) not defined error
+//  c) null
+//  d) 0
+
+// b . not defind
+
+
+// 7. What is the output?
+//  var x;
+//  console.log(x);
+//  a) 0
+//  b) undefined
+//  c) not defined
+//  d) null
+
+// b . undefind
+
+
+// 8. Which of the following keywords is used for variable hoisting?
+//  a) let
+//  b) const
+//  c) var
+//  d) both let and var
+
+// c . var
+
+
+// 9. What is the output?
+//  var num = 20;
+//  var num = 30;
+//  console.log(num);
+//  a) 20
+//  b) 30
+//  c) Error: duplicate declaration
+//  d) undefined
+
+// b . 30
+
+
+// 10. What type of error is 'x is not defined'?
+//  a) SyntaxError
+//  b) ReferenceError
+//  c) TypeError
+//  d) RangeError
+
+// b . reference error
+
+
+// 11. Which of the following is a correct HTML tag for inserting a line break?
+//  a) <break>
+//  b) <lb>
+//  c) <br>
+//  d) <line>
+
+// c . br
+
+
+// 12. HTML stands for:
+//  a) Hyper Transfer Markup Language
+//  b) Hyper Text Markup Language
+//  c) High Text Machine Language
+//  d) Hyper Text Managing Links
+
+// b
+
+
+// 13. Which of the following is a paired tag in HTML?
+//  a) <br>
+//  b) <img>
+//  c) <p>
+//  d) <hr>
+
+// c
+
+
+// 14. The <!DOCTYPE html> declaration defines:
+//  a) The version of JavaScript used
+//  b) The CSS framework used
+//  c) The type of document and HTML version
+//  d) The character encoding
+
+// c
+
+
+// 15. Which of the following is correct about HTML comments?
+//  a) // Comment
+//  b) /* Comment */
+//  c) <!-- Comment -->
+//  d) # Comment
+
+// c
