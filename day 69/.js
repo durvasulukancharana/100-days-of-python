2f1216b8fbf62d5faaaa6460bd44502f8c5593a0
durvasulukancharana <durvasulukancharana25@gmail.com>
2025-09-29 21:40:41 +0530

    day 69 of js

---
+++ day 69/.js
@@ -0,0 +1,261 @@
+// 1. What will be the result of 10 + 5 * 2?
+// a) 30 b) 20 c) 15 d) 25
+
+// b) 20
+
+
+// 2. Which operator is used to compare both value and type in JavaScript?
+// a) == b) === c) = d) !=
+
+// b) ===
+
+
+// 3. What is the result of true && false?
+// a) true b) false c) undefined d) null
+
+// b) false
+
+
+// 4. Which operator is used to assign a value to a variable?
+// a) = b) == c) === d) :=
+
+// a) =
+
+
+// 5. What will 15 % 4 return?
+// a) 2 b) 3 c) 4 d) 1
+
+// b) 3
+
+
+// 6. What is the result of 8 | 3?
+// a) 11 b) 2 c) 0 d) 5
+
+//      8  4  2  1
+// 8    1  0  0  0
+// 3    0  0  1  1
+//      ----------
+//      1  0  1  1
+
+// a) 11
+
+
+// 7. Which operator decreases a number by one?
+// a) ++ b) -- c) -= d) **
+
+// b) b
+
+
+// 8. What will be the result of 5 == '5'?
+// a) true b) false c) error d) NaN
+
+// a) true
+
+
+// 9. What is the result of !true?
+// a) true b) false c) null d) undefined
+
+// b) false
+
+
+// 10. Which operator is used to combine multiple conditions?
+// a) & b) && c) || d) !!
+
+// b) &&
+
+
+// 11. What does 10 ** 2 evaluate to?
+// a) 100 b) 20 c) 102 d) NaN
+
+// a) 100
+
+
+// 12. What is the result of 10 != '10'?
+// a) true b) false c) error d) NaN
+
+// b) false
+
+
+// 13. Which operator represents bitwise AND?
+// a) & b) && c) || d) %
+
+// a) &
+
+
+// 14. What will 7 ^ 2 return?
+// a) 5 b) 2 c) 9 d) 4
+
+//      8  4  2  1 
+// 7    0  1  1  1
+// 2    0  0  1  0
+//      -----------
+//      0  1  0  1
+//      -----------
+// a) 5
+
+
+// 15. Which operator assigns a variable and adds to it?
+// a) = b) += c) == d) ++
+
+// b) +=
+
+
+// 16. What is the result of 20 / 5?
+// a) 4 b) 5 c) 2 d) 0
+
+// a) 4
+
+
+// 17. What does 5 > 3 && 2 < 1 evaluate to?
+// a) true b) false c) null d) error
+
+// b) false
+
+
+// 18. What is the result of 12 & 5?
+// a) 4 b) 5 c) 0 d) 1
+
+// a) 4
+
+
+// 19. Which operator represents logical OR?
+// a) | b) || c) & d) &&
+
+// b) ||
+
+
+// 20. What will 9 % 2 return?
+// a) 0 b) 1 c) 2 d) 9
+
+// b) 1
+
+
+// 21. Which operator is used to increment by 1?
+// a) ++ b) -- c) += d) =
+
+// a) ++
+
+
+// 22. What is the result of false || true?
+// a) true b) false c) null d) undefined
+
+// a) true
+
+
+// 23. Which operator performs bitwise NOT?
+// a) ! b) ~ c) ^ d) &
+
+// a) !
+
+
+// 24. What will 10 >= 5 return?
+// a) true b) false c) NaN d) error
+
+// b) false
+
+
+// 25. What is the result of 4 * 3?
+// a) 7 b) 12 c) 1 d) 0
+
+// b) 12
+
+
+// 26. Which operator checks inequality in value and type?
+// a) != b) !== c) == d) =
+
+// b) !==
+
+
+// 27. What is the result of 2 ** 3?
+// a) 8 b) 6 c) 9 d) 5
+
+// a) 8
+
+
+// 28. Which operator is used for modulus?
+// a) % b) / c) * d) //
+
+// a) %
+
+
+// 29. What will 14 | 1 return?
+// a) 15 b) 1 c) 0 d) 14
+
+//      8  4  2  1
+// 14   1  1  1  0
+// 1    0  0  0  1
+//      ----------
+//      1  1  1  1
+//      ----------
+//a) 15
+
+// 30. What does 10 < 5 || 8 > 6 evaluate to?
+// a) true b) false c) null d) NaN
+
+// a) true
+
+
+// 31. What is the result of 5 & 1?
+// a) 1 b) 5 c) 0 d) 2
+
+//      4  2  1
+// 5    1  0  0
+// 1    0  0  1
+//      -------
+//      0  0  0
+//      -------
+// c) 0
+
+
+// 32. Which operator assigns remainder after division?
+// a) %= b) /= c) *= d) ++
+
+// a) %=
+
+
+// 33. What will 6 === '6' return?
+// a) true b) false c) NaN d) error
+
+// b) false
+
+
+// 34. What is the result of !false?
+// a) true b) false c) null d) undefined
+
+// a) true
+
+
+// 35. Which operator represents bitwise XOR?
+// a) ^ b) && c) || d) ~
+
+// a) ^
+
+
+// 36. What will 100 / 0 return?
+// a) Infinity b) NaN c) 0 d) error
+
+// a) 
+
+
+// 37. What does 7 >= 7 && 5 < 10 evaluate to?
+// a) true b) false c) null d) error
+
+// a) true
+
+
+// 38. What is the result of 1 | 0?
+// a) 1 b) 0 c) NaN d) 2
+
+// a) 1
+
+
+// 39. Which operator multiplies and assigns value?
+// a) *= b) ++ c) ** d) =
+
+// a) *=
+
+
+// 40. What will 8 < 6 return?
+// a) true b) false c) null d) undefined
+
+// b) false
